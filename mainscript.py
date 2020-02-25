6c52cf7dabd7c10d9c2ff07dc5011049caff3131
Satyam Kapoor <satyamkapoor@outlook.com>
2020-02-25 23:57:23 +0100

    added draft script integrated with Telegram

--- mainscript.py
+++ mainscript.py
@@ -1,3 +1,16 @@
 import subprocess
-result = subprocess.run(['arp', '-v'], stdout=subprocess.PIPE)
-raw_data = result.stdout
+import requests
+
+result = subprocess.check_output(["arp -v | grep -v 'Address\|Entries'"], shell=True)
+raw_data = result.decode('utf-8')
+r2=raw_data.split("\n")
+trustedIP=[]
+
+unauth="Unauthorised IP found on the network \n ----------------------- \n"
+myIPlist = ['192.168.1.01', '192.168.1.3'] #list of trsuted IP addresses
+
+for i in range(0,(len(r2)-1)):
+        temp = str(r2[i]).split(' ')
+        if temp[0] not in myIPlist:
+                print(r2[i])
+                requests.get('https://api.telegram.org/bot{INPUT_YOUR_BOT_TOKEN_HERE}/sendMessage?chat_id=370905990&text={0} {1} \n -----------------------'.format(unauth,r2[i]))
